bedf243fd4c23ed72b742a5fa869d4beedae0150
Parlin Yu <bupt.aswmtjdsj@gmail.com>
2014-04-02 19:42:53 -0400

    correct operator -

--- graph_base.cpp
+++ graph_base.cpp
@@ -4,7 +4,7 @@ using namespace cv;
 using namespace std;
 
 Vec3b operator -(const Vec3b &a, const Vec3b &b) {
-    return Vec3b(a.val[0] - b.val[0], a.val[1] - b.val[1], a.val[2] - b.val[2]) ;
+    return Vec3b(abs(a.val[0] - b.val[0]), abs(a.val[1] - b.val[1]), abs(a.val[2] - b.val[2])) ;
 }
 
 Vec3b operator +(const Vec3b &a, const Vec3b &b) {

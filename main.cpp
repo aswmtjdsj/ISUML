07f7f43a6a1a61ecf3bfe5e6a9df09916be7828e
Parlin Yu <bupt.aswmtjdsj@gmail.com>
2014-04-02 20:14:17 -0400

    choose test case or input case

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ int main(int argc, const char ** argv) {
         puts("please tell me the file name and file type of original image!");
         exit(1);
     }
-
-    TestImage test_obj(argv);
-    test_obj.getExt();
-    test_obj.drawTestImage(300, 400);
+    if(argc == 4 && strcmp("test", argv[3]) == 0) {
+        TestImage test_obj(argv);
+        test_obj.getExt();
+        test_obj.drawTestImage(300, 400);
+    }
 
     Segmentation test_seg(argv);
     test_seg.buildGraph();
